b71525d2db33d2309f413c7aac4d77b563bb7570
David Gahnassia <davidgahnassia@gmail.com>
2019-06-19 08:40:08 +0300

    second commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   production: false,
-  firebaseUrl:{
-    apiKey: "AIzaSyA8dWBPgKW7IE-eZGMROuWxn7F8j-y1pXg",
-    authDomain: "agendays-58b47.firebaseapp.com",
-    databaseURL: "https://agendays-58b47.firebaseio.com",
-    projectId: "agendays-58b47",
-    storageBucket: "agendays-58b47.appspot.com",
-    messagingSenderId: "25958110232",
-    appId: "1:25958110232:web:1f2babe016875cd9"
+  firebaseUrl: {
+    apiKey: 'AIzaSyA8dWBPgKW7IE-eZGMROuWxn7F8j-y1pXg',
+    authDomain: 'agendays-58b47.firebaseapp.com',
+    databaseURL: 'https://agendays-58b47.firebaseio.com',
+    projectId: 'agendays-58b47',
+    storageBucket: 'agendays-58b47.appspot.com',
+    messagingSenderId: '25958110232',
+    appId: '1:25958110232:web:1f2babe016875cd9'
   }
 };
 
